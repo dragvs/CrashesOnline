51e4bdfafee9a9bc078b1ccb58143f073f87e0d7
dragvs <dragvs@gmail.com>
2014-02-12 01:14:40 +0400

    [New] Implemented client lib upload [New] Implemented client crash dump upload

--- server/server.js
+++ server/server.js
@@ -7,72 +7,97 @@ var crypto = require('crypto');
 var path = require('path')
 var formidable = require('formidable');
 var fs   = require('fs');
+var exec = require('child_process').exec;
+var mkdirp = require('mkdirp');
 
 
 var server = http.createServer(function(req, res) {
     // Simple path-based request dispatcher
     switch (url.parse(req.url).pathname) {
         case '/':
-            display_form(req, res);
+            display_form(req, res); // TODO remove test form UI
             break;
-        case '/upload':
-            upload_file(req, res);
+        case '/ucl':
+            uploadClientLib(req, res);
+            break;
+        case '/ucd':
+            uploadClientDump(req, res);
             break;
         default:
-            show_404(req, res);
+            sendTextResponse(res, "You'r doing it wrong!", 404);
             break;
     }
 });
 
 var configFilePath = __dirname + '/config.json';
-console.log("Config file path: " + configFilePath);
-
-var targetUploadPath    = __dirname + "/uploaded";
-var tempUploadPath      = __dirname + "/tmp_upload";
-
 var configData;
 
-fs.readFile(configFilePath, 'utf8', function (err, data) {
-    if (err) {
-        console.log('Error reading config file: ' + err);
-        return;
+var dumpSymsToolPath = __dirname + "/../tools/google-breakpad/mac/dump_syms";
+
+var workingFolderPath       = __dirname + "/test1";
+var symbolsFolderPath       = workingFolderPath + "/symbols";
+var clientLibTargetPath     = workingFolderPath + "/clientLibUpload";
+var clientLibTempPath       = workingFolderPath + "/tmp_clientLibUpload";
+var clientDumpTargetPath    = workingFolderPath + "/dumpUpload";
+var clientDumpTempPath      = workingFolderPath + "/tmp_dumpUpload";
+
+
+main();
+
+function main() {
+    fs.readFile(configFilePath, 'utf8', function (err, data) {
+        if (err) {
+            console.log('Error reading config file: ' + err);
+            return;
+        }
+     
+        configData = JSON.parse(data);
+        console.dir(configData);
+
+        console.log("New API key: " + generateApiKey());
+
+        checkAndCreateDir(clientLibTargetPath);
+        checkAndCreateDir(clientLibTempPath);
+        checkAndCreateDir(clientDumpTargetPath);
+        checkAndCreateDir(clientDumpTempPath);
+
+        // Server would listen on port 80
+        server.listen(80);
+    });
+}
+
+function checkAndCreateDir(path) {
+    if (!fs.existsSync(path)) {
+        // fs.mkdirSync(path);
+        mkdirp.sync(path);
+        console.log("Created dir: " + path);
     }
- 
-    configData = JSON.parse(data);
-    console.dir(configData);
+}
 
-    console.log("New API key: " + generateApiKey());
-
-    // Server would listen on port 80
-    server.listen(80);
-});
+function getFileNameWithoutExt(filePath) {
+    var extname = path.extname(filePath);
+    var filename = path.basename(filePath, extname);
+    return filename;
+}
 
 /*
  * Display upload form
  */
 function display_form(request, response) {
     var body = '<h1>Hello!</h1>'+
-        '<form action="/upload" method="post" enctype="multipart/form-data">'+
+        '<form action="/ucl" method="post" enctype="multipart/form-data">'+
         '<input type="text" name="apiKey" style="width: 750px;" value="Itp9g4219uvmPxXYUmR546VjbXrJGknhdh5GY72gUoGxujzHbczj31PNKsXE25rYCS0ukQyGyCOX9IbszYzq3A=="><br/>'+
         '<input type="text" name="appId" style="width: 250px;" value="com.redsteep.simpleapp"><br/>'+
         '<input type="text" name="appVersion" value="1.0 dev-1"><br/>'+
-        '<input type="file" name="upload-file">'+
+        '<input type="file" name="upload-file" style="width: 400px;">'+
         '<input type="submit" value="Upload">'+
         '</form>';
-    response.writeHead(200, {
-      'Content-Length': body.length,
-      'Content-Type': 'text/html' });
-    response.end(body);
+    sendHtmlResponse(response, body, 200);
 }
 
-/*
- * Handle file upload
- */
-function upload_file(request, response) {
-    // if (request.method.toLowerCase() == 'post')
-
+function createIncomingForm(uploadDir) {
     var form = new formidable.IncomingForm();
-    form.uploadDir = tempUploadPath;
+    form.uploadDir = uploadDir;
 
     form.on('fileBegin', function(name, file) {
         console.log("Form on File begin: " + file.name);
@@ -89,18 +114,21 @@ function upload_file(request, response) {
     form.on('error', function(err) {
         console.error("Form error: " + err);
     });
+    return form;
+}
+
+/*
+ * Handle file upload
+ */
+function uploadClientLib(request, response) {
+    // if (request.method.toLowerCase() == 'post')
+
+    var uploadTargetPath = clientLibTargetPath;
+
+    var form = createIncomingForm(clientLibTempPath);
 
     form.parse(request, function(err, fields, files) {
-        console.log("Form on Parse");
-
-        var file = files['upload-file'];
-
-        var extname = path.extname(file.path);
-        var filename = path.basename(file.path, extname);
-
-        var newFilePath = targetUploadPath + "/" + file.name + "." + filename;
-
-        console.log("   File target path " + newFilePath);
+        console.log("Form[client lib] on Parse");
 
         var apiKey = fields['apiKey'];
         var appId = fields['appId'];
@@ -113,13 +141,178 @@ function upload_file(request, response) {
         var appConfig = findAppConfig(apiKey) //configData[apiKey];
         
         if (!appConfig || appConfig.appId != appId) {
-            var body = "ApiKey or App id is incorrect!";
-            response.writeHead(200, {
-              'Content-Length': body.length,
-              'Content-Type': 'text/plain' });
-            response.end(body);
+            sendTextResponse(response, "ApiKey or App id is incorrect!", 403);
             return;
         }
+        if (!appConfig.libUpload) {
+            sendTextResponse(response, "Lib upload is restricted!", 403);
+            return;
+        }
+
+        // Write META
+        var file = files['upload-file'];
+        var tmpFileName = getFileNameWithoutExt(file.path);
+
+        var metaData = getClientMeta(fields, appConfig, appVersion);
+        var metaFilePath = uploadTargetPath + "/" + file.name + "." + tmpFileName + ".meta";
+
+        fs.writeFile(metaFilePath, metaData, function (err2) {
+            if (err2) {
+                console.error("Saving client lib META error: " + err2);
+            } else {
+                console.log('Client lib META saved');
+            }
+        });
+
+        // Write file
+        var newFilePath = uploadTargetPath + "/" + file.name + "." + tmpFileName;
+        console.log("   File target path " + newFilePath);
+
+        fs.rename(file.path, newFilePath, function(err2) {  
+            if (err2) {
+                console.error("File rename error: " + err2);
+            } else {
+                processClientLib(newFilePath);
+                console.log("Uploaded tmp file renamed");
+            }
+        });
+
+        // Render response
+        sendTextResponse(response, "Operation: OK", 200);
+    });
+}
+
+function processClientLib(libPath) {
+    var symFilePath = libPath + ".sym";
+    var command = dumpSymsToolPath + " " + libPath + " > " + symFilePath;
+
+    console.log("::processClientLib executing dump_syms command: " + command);
+
+    var child1 = exec(command, function (error, stdout, stderr) {
+        console.log("::processClientLib dump_syms exec finished");
+
+        if (error !== null) {
+            console.error('::processClientLib dump_syms exec error: ' + error);
+            console.log("::processClientLib dump_syms exec stderr: " + stderr);
+        } else {
+            console.log("::processClientLib dump_syms exec stderr: " + stderr);
+
+            var headCommand = "head -n1 " + symFilePath;
+            console.log("::processClientLib executing head command: " + headCommand);
+
+            var child2 = exec(headCommand, function(error, stdout, stderr) {
+                console.log("::processClientLib head exec finished");
+                console.log("::processClientLib head exec stdout: " + stdout);
+                console.log("::processClientLib head exec stderr: " + stderr);
+
+                if (error !== null) {
+                    console.error('::processClientLib head exec error: ' + error);
+                } else {
+                    // MODULE <OS> <arch> <so_uuid> <so_name>
+                    var headerArr = stdout.split(" ");
+
+                    if (headerArr.length != 5)
+                        console.error('Expected sym file header with 5 elements, found: ' + headerArr.length);
+                    if (headerArr[0] != "MODULE")
+                        console.error("Expected 'MODULE' field in sym file header, found: " + headerArr[0]);
+
+                    var os = headerArr[1];
+                    var arch = headerArr[2];
+                    var soUuid = headerArr[3];
+                    var soName = headerArr[4];
+
+                    console.log("SYM file header: " + os + " " + arch + " " + soUuid + " " + soName);
+
+                    var soFileName = getFileNameWithoutExt(libPath);
+
+                    // symbols/<so_name>/<so_uuid>/<so_name>.sym
+                    var symFileFolderPath = symbolsFolderPath + "/" + soFileName + "/" + soUuid;
+                    var symFileTargetPath = symFileFolderPath + "/" + soFileName + ".sym";
+
+                    console.log("SYM file target path: " + symFileTargetPath);
+
+                    if (fs.existsSync(symFileTargetPath)) {
+                        var newStat = fs.statSync(symFilePath);
+                        var oldStat = fs.statSync(symFileTargetPath);
+
+                        if (newStat.size == oldStat.size) {
+                            console.log("Found SYM file with same UUID (" + soUuid + 
+                                ") and file size: " + oldStat.size + " bytes");
+                        } else {
+                            console.log("ERROR: Found SYM file with same UUID (" + soUuid + 
+                                ") but different size: " + newStat.size + " bytes instead of " + 
+                                oldStat.size + " bytes");
+                        }
+                    } else {
+                        checkAndCreateDir(symFileFolderPath);
+
+                        fs.rename(symFilePath, symFileTargetPath, function(err2) {  
+                            if (err2) {
+                                console.error("Failed to move SYM file to target path: " + err2);
+                            } else {
+                                console.log("SYM file moved to target path: " + symFileTargetPath);
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    });
+}
+
+/*
+ * Handle dump upload
+ */
+function uploadClientDump(request, response) {
+    // if (request.method.toLowerCase() == 'post')
+
+    var uploadTargetPath = clientDumpTargetPath;
+
+    var form = createIncomingForm(clientDumpTempPath);
+
+    form.parse(request, function(err, fields, files) {
+        console.log("Form[client dump] on Parse");
+
+        var apiKey = fields['apiKey'];
+        var appId = fields['appId'];
+        var appVersion = fields['appVersion'];
+
+        console.log("   API key: " + apiKey);
+        console.log("   App id: " + appId);
+        console.log("   App version: " + appVersion);
+
+        var appConfig = findAppConfig(apiKey) //configData[apiKey];
+        
+        if (!appConfig || appConfig.appId != appId) {
+            sendTextResponse(response, "ApiKey or App id is incorrect!", 403);
+            return;
+        }
+        if (!appConfig.dumpUpload) {
+            sendTextResponse(response, "Dump upload is restricted!", 403);
+            return;
+        }
+
+        // Write META
+        var file = files['upload-file'];
+        var tmpFileName = getFileNameWithoutExt(file.path);
+
+        var appSubdirPath = uploadTargetPath + "/" + appConfig.description;
+        checkAndCreateDir(appSubdirPath);
+
+        var metaData = getClientMeta(fields, appConfig, appVersion);
+        var metaFilePath = appSubdirPath + "/" + file.name + "." + tmpFileName + ".meta";
+
+        fs.writeFile(metaFilePath, metaData, function (err2) {
+            if (err2) {
+                console.error("Saving client dump META error: " + err2);
+            } else {
+                console.log('Client dump META saved');
+            }
+        });
+
+        // Write file
+        var newFilePath = appSubdirPath + "/" + file.name + "." + tmpFileName;
+        console.log("   File target path " + newFilePath);
 
         fs.rename(file.path, newFilePath, function(err2) {  
             if (err2) {
@@ -130,26 +323,42 @@ function upload_file(request, response) {
         });
 
         // Render response
-        var body = "Thanks for playing!\n";
-        body += "received upload:\n\n";
-        body += util.inspect({fields: fields, files: files});
-
-        response.writeHead(200, {
-          'Content-Length': body.length,
-          'Content-Type': 'text/plain' });
-        response.end(body);
+        sendTextResponse(response, "Operation: OK", 200);
     });
 }
 
-/*
- * Handles page not found error
- */
-function show_404(request, response) {
-    var body = "You'r doing it wrong!";
-    response.writeHead(404, {
-      'Content-Length': body.length,
+function getClientMeta(fields, appConfig, appVersion) {
+    var meta = "ApiKey: " + appConfig.apiKey + "\n";
+    meta += "AppId: " + appConfig.appId + "\n";
+    meta += "AppVersion: " + appVersion + "\n";
+
+    meta += "ID: " + fields['meta.ID'] + "\n";
+    meta += "MANUFACTURER: " + fields['meta.MANUFACTURER'] + "\n";
+    meta += "MODEL: " + fields['meta.MODEL'] + "\n";
+    meta += "PRODUCT: " + fields['meta.PRODUCT'] + "\n";
+    meta += "VERSION.CODENAME: " + fields['meta.VERSION.CODENAME'] + "\n";
+    meta += "VERSION.INCREMENTAL: " + fields['meta.VERSION.INCREMENTAL'] + "\n";
+    meta += "VERSION.RELEASE: " + fields['meta.VERSION.RELEASE'] + "\n";
+    meta += "VERSION.SDK_INT: " + fields['meta.VERSION.SDK_INT'];
+    return meta;
+}
+
+function sendTextResponse(response, text, statusCode) {
+    statusCode = typeof statusCode !== 'undefined' ? statusCode : 200;
+
+    response.writeHead(statusCode, {
+      'Content-Length': text.length,
       'Content-Type': 'text/plain' });
-    response.end(body);
+    response.end(text);
+}
+
+function sendHtmlResponse(response, text, statusCode) {
+    statusCode = typeof statusCode !== 'undefined' ? statusCode : 200;
+
+    response.writeHead(statusCode, {
+      'Content-Length': text.length,
+      'Content-Type': 'text/html' });
+    response.end(text);
 }
 
 function findAppConfig(apiKey) {
